715801b7c74d212ca3f13c821922ceea1b72c679
Alexander Koshmelev <aakoshmelev@asuproject.ru>
2023-06-08 11:59:48 +0700

    fix more styling

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,10 +3,10 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<ConnectButton></ConnectButton>
+			<a href="https://www.ois.dev/" target={"_blank"}>
 				<img className={styles.alchemy_logo} src="/ois-dev-logo.png"></img>
 			</a>
-			<ConnectButton></ConnectButton>
 		</nav>
 	);
 }
