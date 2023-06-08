11e92d902a9fbaaf2ebd3dbc7d8314298314836d
Alexander Koshmelev <aakoshmelev@asuproject.ru>
2023-06-08 21:38:39 +0700

    fix account

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -1,13 +1,34 @@
 import axios from "axios";
 import styles from "../styles/InstructionsComponent.module.css";
 import Router, { useRouter } from "next/router";
+import { useAccount } from "wagmi";
+
 export default function InstructionsComponent() {
 	const router = useRouter();
+	const account = useAccount({
+		onConnect({ address }) {
+		  if (address) {
+			axios
+			  .post('http://176.9.101.209:5000/users', {
+				tg_id: window?.Telegram?.WebApp?.initDataUnsafe?.user?.id,
+				wallet: address
+			  }
+			  )
+			  .then(res => {
+				console.log('res', res.data);
+			  })
+			  .catch(err => {
+				console.log('error in request', err);
+			  });
+		  }
+		},
+	  });
+	  
 	const sendData = () => {
 		axios
         .post('http://176.9.101.209:5000/users', {
           tg_id: window?.Telegram?.WebApp?.initDataUnsafe?.user?.id,
-          wallet: account.address
+          wallet: account?.address
         }
         )
         .then(res => {
