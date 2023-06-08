164e73cf7e30eae68bc7e14f2ace84ef936fcbeb
Alexander Koshmelev <aakoshmelev@asuproject.ru>
2023-06-08 10:43:08 +0700

    first change

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -6,63 +6,21 @@ export default function InstructionsComponent() {
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					create<span>-web3-dapp</span>
+					telegram<span>-sniper-bot</span>
 				</h1>
-				<p>
-					Get started by editing this page in{" "}
-					<span>/pages/index.js</span>
-				</p>
 			</header>
 
 			<div className={styles.buttons_container}>
-				<a
-					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#components"}
-				>
-					<div className={styles.button}>
-						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Add Components</p>
-					</div>
-				</a>
-				<a
-					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#templates"}
-				>
-					<div className={styles.button}>
-						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Explore Templates</p>
-					</div>
-				</a>
-				<a
-					target={"_blank"}
-					href={"https://docs.alchemy.com/docs/create-web3-dapp"}
-				>
-					<div className={styles.button}>
-						<img
-							src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
-							width={"20px"}
-							height={"20px"}
-						/>
-						<p>Visit Docs</p>
-					</div>
-				</a>
+				
 			</div>
 			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-					<img
-						id="badge-button"
-						style={{ width: "240px", height: "53px" }}
-						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
-						alt="Alchemy Supercharged"
-					/>
-				</a>
 				<div className={styles.icons_container}>
 					<div>
 						<a
 							href="https://github.com/alchemyplatform/create-web3-dapp"
 							target={"_blank"}
 						>
-							Leave a star on Github
+							Leave us a star on Clutch
 						</a>
 					</div>
 					<div>
@@ -70,7 +28,7 @@ export default function InstructionsComponent() {
 							href="https://twitter.com/AlchemyPlatform"
 							target={"_blank"}
 						>
-							Follow us on Twitter
+							Follow us on LinkedIn
 						</a>
 					</div>
 				</div>
